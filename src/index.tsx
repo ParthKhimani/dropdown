4a45c50c9b0d296c79e6cf9c781deb1f329b6ae2
Parth Khimani <parthkhimani48@gmail.com>
2023-05-18 15:50:18 +0530

    few changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import CustomCheckbox from './components/CheckBox/CustomCheckbox';
 import CustomInput from './components/Inputs/CustomInput';
+import CustomRadio from './components/Radio/CustomRadio';
 import Dropdown from './Dropdown';
 
-export { Dropdown, CustomInput, CustomCheckbox };
+export { Dropdown, CustomInput, CustomCheckbox, CustomRadio };
